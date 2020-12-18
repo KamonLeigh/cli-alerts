0f700afd901dfe6a3ad719006dd22519f7fe31cb
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2020-12-18 00:10:51 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * CLI Alerts
+ * 
+ * Cross platform CLI Alerts with colours
+ * works on macOS, Windows and Linux
+ * Alerts `success`, `info`, `warning`, `error`
+ * 
+ * 
+ * @author Byron <byronleigh80gmail.com>
+ */
